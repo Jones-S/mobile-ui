310bf8c483a4938e761931c477729862e7150040
Jones <jonas.scheiwiller@zhdk.ch>
2015-10-06 13:36:40 +0200

    [update] add angular js setup example page in comment

--- js/controllers/exerciseController.js
+++ js/controllers/exerciseController.js
@@ -1,3 +1,6 @@
+// http://www.ng-newsletter.com/posts/beginner2expert-scopes.html
+
 app.controller('exerciseController', ['$scope', function($scope) {
     $scope.title = 'Top Sellers in Books 22';
 }]);
+
